5d32f873a9f4207b081bb18b4b27f79bee6e1e13
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2023-07-14 17:30:01 +0200

    added testing shopping lists

---
+++ src/app/shopping-list/data.ts
@@ -0,0 +1,87 @@
+import ShoppingImage from '../../../public/images/shopping-list.jpg';
+import ShoppingImage2 from '../../../public/images/shopping-list-2.jpg';
+
+const shoppingItems = [
+  { id: crypto.randomUUID(), name: 'chleb', price: 3, currency: 'PLN', category: 'pieczywo', image: ShoppingImage },
+  {
+    id: crypto.randomUUID(),
+    name: 'masło',
+    price: 5,
+    currency: 'PLN',
+    category: 'produkty mleczne',
+    image: ShoppingImage2
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'drożdżówka',
+    price: 2,
+    currency: 'PLN',
+    category: 'pieczywo',
+    image: ShoppingImage
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'ser',
+    price: 2,
+    currency: 'PLN',
+    category: 'pieczywo',
+    image: ShoppingImage
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'kiełba',
+    price: 2,
+    currency: 'PLN',
+    category: 'pieczywo',
+    image: ShoppingImage
+  }
+];
+
+const shoppingItems2 = [
+  { id: crypto.randomUUID(), name: 'chleb', price: 3, currency: 'PLN', category: 'pieczywo', image: ShoppingImage },
+  {
+    id: crypto.randomUUID(),
+    name: 'margaryna',
+    price: 2,
+    currency: 'PLN',
+    category: 'produkty mleczne',
+    image: ShoppingImage2
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'jagodzianka',
+    price: 3,
+    currency: 'PLN',
+    category: 'pieczywo',
+    image: ShoppingImage
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'ser',
+    price: 2,
+    currency: 'PLN',
+    category: 'pieczywo',
+    image: ShoppingImage
+  },
+  {
+    id: crypto.randomUUID(),
+    name: 'kiełba',
+    price: 2,
+    currency: 'PLN',
+    category: 'pieczywo',
+    image: ShoppingImage
+  }
+];
+
+export const lists = [{
+  id: crypto.randomUUID(),
+  items: shoppingItems,
+  image: ShoppingImage
+},
+{
+  id: crypto.randomUUID(),
+  items: shoppingItems2,
+  image: ShoppingImage2
+
+},
+];
