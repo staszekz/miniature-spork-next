cdb872669bba30b25ad056d8cc4d4653af4e22fb
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2023-11-15 14:20:36 +0100

    feat: 🎸 sag
    sg
    
    BREAKING CHANGE: 🧨 dsghs
    
    ✅ Closes: ergh

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body className="antialiased">
         <MantineProvider>
+          {console.log('children', children)}
           <Layout>{children}</Layout>
         </MantineProvider>
       </body>
