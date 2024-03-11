5062dfae9333c8f6d76445feb797438e8900f64e
Staszek Zajaczkowski <staszek.zajaczkowski@gmail.com>
2024-03-10 23:03:05 -0400

    added first post to sanity and instal sanity client

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-'use client';
-
 import { Button, Center, Container } from '@mantine/core';
+import { createClient } from '@sanity/client';
 import Head from 'next/head';
 import Link from 'next/link';
 
-export default function Home() {
+const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // you can find this in sanity.json
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET, // you can find this in sanity.json
+  useCdn: true // `false` if you want to ensure fresh data
+});
+const query = `*[_type == "post"]{
+  title,
+  "categories": categories[]->title,
+  "author": author->{slug, name},
+  "bodyText": body[]{
+    "paragraph": children[0].text
+  }
+}`;
+
+export default async function Home() {
+  const data = await client.fetch(query);
+  console.log(data[0].bodyText);
+
   return (
     <>
       <Head>
@@ -16,6 +32,10 @@ export default function Home() {
           <Button color="teal" component={Link} href="/shopping-lists">
             Zobacz swoje listy zakupów
           </Button>
+
+          {/* <Container>{title}</Container> */}
+          {/* <Container>{slug}</Container> */}
+          {/* <Container>{title}</Container> */}
         </Center>
       </Container>
     </>
